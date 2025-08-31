012f3858f904b5f3470888d0fb023f8dc3af3afa
Harsh <harsh.bhumihar7@gmail.com>
2025-08-31 15:28:27 +0530

    implemented cross hosting

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import { initLogger } from "./utils/logger.js";
 import userRoutes from "./routes/user_routes.js";
 import templateRoutes from "./routes/template_routes.js";
 import planRoutes from "./routes/plan_routes.js";
+import path from "path";
+const __dirname = path.resolve();
 
 const app = express();
 app.use(initLogger);
@@ -10,5 +12,9 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/templates", templateRoutes);
 app.use("/api/plans", planRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 export default app;
