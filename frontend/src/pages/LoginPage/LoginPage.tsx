2e515caa1cbff65f52da2f9410b2bd560172d261
Harsh <harsh.bhumihar7@gmail.com>
2025-11-06 20:27:18 +0530

    added demo page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,53 +1,51 @@
-import z from "zod";
 import { useState, type FormEvent } from "react";
 import PrimaryButton from "../../components/ui/Button/PrimaryButton";
 import SecondaryButton from "../../components/ui/Button/SecondaryButton";
 import Field from "../../components/ui/Field/Field";
-import { loginSchema } from "../../utils";
 
-type LoginFormData = z.infer<typeof loginSchema>;
+// type LoginFormData = z.infer<typeof loginSchema>;
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [validationError, setValidationError] = useState<{
-    email?: string;
-    password?: string;
-  }>({});
+  // const [validationError, setValidationError] = useState<{
+  //   email?: string;
+  //   password?: string;
+  // }>({});
 
-  const validateForm = (): boolean => {
-    try {
-      loginSchema.parse({ email, password });
-      setValidationError({});
-      return true;
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        const errors: { email?: string; password?: string } = {};
-        err.issues.forEach((error) => {
-          if (error.path[0] === "email") {
-            errors.email = error.message;
-          } else if (error.path[0] === "password") {
-            errors.password = error.message;
-          }
-        });
-        setValidationError(errors);
-      }
-      return false;
-    }
-  };
+  // const validateForm = (): boolean => {
+  //   try {
+  //     loginSchema.parse({ email, password });
+  //     setValidationError({});
+  //     return true;
+  //   } catch (err) {
+  //     if (err instanceof z.ZodError) {
+  //       const errors: { email?: string; password?: string } = {};
+  //       err.issues.forEach((error) => {
+  //         if (error.path[0] === "email") {
+  //           errors.email = error.message;
+  //         } else if (error.path[0] === "password") {
+  //           errors.password = error.message;
+  //         }
+  //       });
+  //       setValidationError(errors);
+  //     }
+  //     return false;
+  //   }
+  // };
 
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     event.preventDefault();
-    if (!validateForm()) return;
-    try {
-    } catch (error) {}
+    // if (!validateForm()) return;
+    // try {
+    // } catch (error) {}
   }
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="w-80 flex flex-col items-start justify-start mb-10 font-semibold text-2xl ">
         <h2 className="text-gray-800">Your Success Architect</h2>
-        <h1 className="text-gray-500">Login to Plagen</h1>
+        <h1 className="text-gray-500">Login to Plangen</h1>
       </div>
       <form action="" onSubmit={handleSubmit}>
         <SecondaryButton className="w-80 mb-2" onClick={() => {}}>
@@ -58,7 +56,12 @@ const LoginPage = () => {
           />
           <span className="ml-15">Continue with Google</span>
         </SecondaryButton>
-        <SecondaryButton className="w-80 mb-2" onClick={() => {}}>
+        <SecondaryButton
+          className="w-80 mb-2"
+          onClick={() => {
+            console.log(`${email} ${password}`);
+          }}
+        >
           <img
             src="https://cdn-icons-png.flaticon.com/128/0/747.png"
             alt="apple"
