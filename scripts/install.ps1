e72b97e16fc02bf333f01b7a000d896e32d51875
Harsh <harsh.bhumihar7@gmail.com>
2025-11-29 11:20:52 +0530

    Commented scription

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -12,10 +12,10 @@ function Install-Service{
     Start-Sleep -Seconds $DelaySeconds
 }
 
-# Install-Service "plan-service" "cd backend/services/plan-service; python -m venv venv; pip install -r requirements.txt"
-# Install-Service "template-service" "cd backend/services/template-service; python -m venv venv; pip install -r requirements.txt"
+Install-Service "plan-service" "cd backend/services/plan-service; python -m venv venv; pip install -r requirements.txt"
+Install-Service "template-service" "cd backend/services/template-service; python -m venv venv; pip install -r requirements.txt"
 Install-Service "auth-service" "cd backend/services/auth-service; npm install; npx prisma generate; npx prisma db push; npm run build"
-# Install-Service "api-gateway" "cd backend/api-gateway; npm install; npm run build"
-# Install-Service "frontend" "cd frontend; npm run dev"
+Install-Service "api-gateway" "cd backend/api-gateway; npm install; npm run build"
+Install-Service "frontend" "cd frontend; npm run dev"
 
 Write-Host "All service started" -ForegroundColor Green
